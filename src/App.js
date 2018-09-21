d00fe52cab0f2f6b51a1de0574789e9cb47f910e
Rodrigo H. A. Xavier <rodrigoxavieer@Rodrigos-MacBook-Pro.local>
2018-09-21 20:21:24 -0300

    Preparando a aula

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {
   View
 } from 'react-native';
 
+import firebase from 'firebase';
+
 import Router from './Router';
 
 const customTextProps = {
@@ -18,7 +20,17 @@ console.disableYellowBox = true;
 
 class App extends Component {
   componentWillMount () {
-    //Posso Fazer qualquer tipo de configuração global aqui
+    //Posso Fazer qualquer tipo de configuração global aqui como por exemplo o Firebase
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyDMQfC7QQArHtedDhUmQtaF-MHy5usQPjI",
+        authDomain: "aulareact-f3277.firebaseapp.com",
+        databaseURL: "https://aulareact-f3277.firebaseio.com",
+        projectId: "aulareact-f3277",
+        storageBucket: "aulareact-f3277.appspot.com",
+        messagingSenderId: "674457894840"
+      })
+    }
   }
 
   render() {
--- src/containers/AuthContainer/Login.js
+++ src/containers/AuthContainer/Login.js
@@ -7,7 +7,11 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, Alert, TextInput, Dimensions} from 'react-native';
+
+var {height, width} = Dimensions.get('window');
+
+import { Actions } from 'react-native-router-flux';
 
 const instructions = Platform.select({
   ios: 'Texto IOS',
@@ -17,15 +21,43 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Lalalala",
+      deviceWidth: width,
+      deviceHeight: height
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Meu Texto aqui</Text>
-        <Text style={styles.instructions}>Com minhas personalizações aqui</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <TextInput
+          style={{height: this.state.deviceHeight * 0.2, width: this.state.deviceWidth * 0.9, borderBottomColor: 'gray', borderBottomWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
       </View>
     );
   }
+
+  openSignUp(){
+    Actions.signup();
+    //Alert.alert("Oba!!", "Vou abrir a tela");
+    /*Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')},
+      ],
+      { cancelable: false }
+    )*/
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -34,6 +66,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  mainButton:{
+    backgroundColor: "#4f8942",
+  },
+  textButton: {
+    color: "white",
+    margin: 20
   },
   welcome: {
     fontSize: 20,
